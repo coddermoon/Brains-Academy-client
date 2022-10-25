76e92cb62caaea0b8b07e58cc7efe361c3a6082d
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-25 15:44:47 +0600

    loded categories done

--- src/pages/Courses/CoursesCard/CourseCard.js
+++ src/pages/Courses/CoursesCard/CourseCard.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const CourseCard = () => {
+
+    const [categories,setCategories]= useState([])
+
+    useEffect(()=>{
+        fetch('https://learning-platform-assignment.vercel.app/categories')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    },[])
+
+    console.log(categories)
+
     return (
-        <div>
+        <div className='mt-5'>
+            <div className="courseCategory text-center">
+
+{
+    categories.map(category=> <Button
+    key={category.category}
+    variant="outline-primary"
+    className='mx-2'
+    >{category.name}</Button>)
+}
+
+        
+
+            </div>
             thsi course card
         </div>
     );
