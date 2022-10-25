5c9923ba4cd4b9004d66c464b3af4d3f2f3a183c
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-25 13:23:37 +0600

    banner section done

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import {FaArrowRight } from 'react-icons/fa';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <section id='banner'>
+         <Container>
+         <Row>
+            <Col  className="d-flex align-items-center" lg="6">
+<div>
+<h1>Envesting in Knowlwdge and <span className='title'>Your Future</span></h1>
+<p>With the helping of E-learning,Create Your own path and drive your skill in your own achive</p>
+
+<button className="mainBtn">Enroll Now <FaArrowRight/> </button>
+</div>
+
+            </Col>
+            <Col lg="6">
+              <div  className="d-flex align-items-center">
+              <img src="https://img.freepik.com/free-vector/hand-holding-smartphone-with-tutor-screen_1262-20644.jpg" alt="prof" />
+              </div>
+            </Col>
+          </Row>
+         </Container>
+        </section>
+    );
+};
+
+export default Banner;
